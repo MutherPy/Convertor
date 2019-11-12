c46ab3e84fd75174cfb213117239a35e812aa59a
MutherPy <MutherPy@users.noreply.github.com>
2019-11-12 03:39:37 +0200

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/second/ExampleUnitTest.java
+++ app/src/test/java/com/example/second/ExampleUnitTest.java
@@ -4,14 +4,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 public class ExampleUnitTest {
+
+    MainActivity ma = new MainActivity();
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void convertation_is_correct() {
+        assertEquals(32.0f, ma.celstofor(0f), 0.0f);
     }
 }
